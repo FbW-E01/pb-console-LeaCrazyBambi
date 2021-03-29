03704866ae4a3158aac2c356dad1e276f8f35f29
LeaCrazyBambi <lheynitz@gmail.com>
2021-03-29 14:00:11 +0200

    I am confused

---
+++ getting-started.js
@@ -0,0 +1,24 @@
+// console.log("this is an exercise in console logging");
+
+// const message = "This is the value";
+
+// console.log(message);
+
+// const firstName = "Lea";
+// const lastName = "Heynitz";
+
+// console.log(firstName, lastName);
+
+// const cityName = "Berlin";
+// const countryName = "Germany";
+// const birthDate = "23.11.1991"
+// const age = "29";
+// const profession = "student"; 
+
+// console.log(cityName,countryName,birthDate,age,profession);
+
+const fullname = "John Smith";
+const age = "43";
+const profession = "teacher";
+const cityName = "Berlin";
+console.log(fullname + " is a " + age + " year old " +profession+" who lives in "+cityName)
